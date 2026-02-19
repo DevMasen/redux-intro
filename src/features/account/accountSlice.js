c43d023402ebd8e6f86dd983bb0cbb1c52d4720f
DevMasen <mohammadwh400@gmail.com>
2026-02-19 20:29:54 +0330

    E276: feat: async action with reudux toolkit

--- src/features/account/accountSlice.js
+++ src/features/account/accountSlice.js
@@ -14,6 +14,7 @@ const accountSlice = createSlice({
 	reducers: {
 		deposit(state, action) {
 			state.balance += action.payload;
+			state.isLoading = false;
 		},
 		withdraw(state, action) {
 			state.balance -= action.payload;
@@ -31,16 +32,37 @@ const accountSlice = createSlice({
 				state.balance += action.payload.amount;
 			},
 		},
-		payLoan(state, _) {
+		payLoan(state) {
 			state.balance -= state.loan;
 			state.loan = 0;
 			state.loanPurpose = '';
 		},
+		converting(state) {
+			state.isLoading = true;
+		},
 	},
 });
 
-export const { deposit, withdraw, requestLoan, payLoan } = accountSlice.actions;
+export const { withdraw, requestLoan, payLoan } = accountSlice.actions;
 export default accountSlice.reducer;
+
+//! We shoule use custom Action Creator for Async Actions
+export function deposit(amount, currency) {
+	if (currency === 'USD') return { type: 'account/deposit', payload: amount };
+	return async function (dispatch, _) {
+		dispatch({ type: 'account/converting' });
+		try {
+			const res = await fetch(
+				`https://api.frankfurter.app/latest?amount=${amount}&from=${currency}&to=USD`,
+			);
+			const data = await res.json();
+			const converted = data.rates.USD;
+			dispatch({ type: 'account/deposit', payload: converted });
+		} catch {
+			throw new Error('Failed to Fetch!');
+		}
+	};
+}
 
 //! Classic Redux Slice
 // export default function AccountReducer(state = initialStateAccount, action) {
